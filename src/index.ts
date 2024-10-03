85e586d6a91133edd7cf36bec4cee877ca2f87d1
Alessandro Bellu <alessandrobellu@hotmail.com>
2024-10-03 21:37:54 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import path from 'path';
+
+import recipeRouter from './routes/recipes';
+import recipesService from './services/recipesService';
+import recipesForIngredientsRouter from './routes/recipesForIngredients';
+
+import ingredientRouter from './routes/ingredients';
+import ingredientsService from './services/ingredientsService';
+
+// Initialise to load large json files in memory
+recipesService.getEntries();
+ingredientsService.getEntries();
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+import cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use((cors as (options: cors.CorsOptions) => express.RequestHandler)({}));
+
+const PORT = 3000;
+
+app.get('/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.use('/api/recipes', recipeRouter);
+app.use('/api/ingredients', ingredientRouter);
+app.use('/api/recipes/forIngredients', recipesForIngredientsRouter);
+
+app.use('/img', express.static(path.join(__dirname, 'public/ingredient-images')));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
