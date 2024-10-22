df12412ff38c979520ee690f3058706aa6f04310
Alessandro Bellu <alessandrobellu@hotmail.com>
2024-10-22 09:39:01 +0100

    First commit

---
+++ app/server/src/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+// var path = require('path');
+var router = express.Router();
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index');
+// });
+
+// router.get('/', (req,res) => {
+//   res.render(path.join(__dirname, '../../../build/index.html'));
+// });  
+
+module.exports = router;
